bc7169fb962e92f3659b93d74e7353db3cc2a6a3
John Leon <50084164+MrPool702@users.noreply.github.com>
2021-11-23 20:56:14 -0500

    Close #33

--- GenerateGraph.py
+++ GenerateGraph.py
@@ -23,64 +23,6 @@ def readNodes(filename):
             arr.append(Point(int(lista[1]),int(lista[2]),lista[3],int(lista[0])))
     return arr
 
-def PuntosEntregasXintegrante():
-    with open("PuntoEntrega.csv", "r") as f:
-        lines = f.readlines()
-        lines.pop(0)
-        PuntosEntrega = []
-        i = 0
-        for line in lines:
-            PuntosEntrega.append(int(line.split(",")[0]))
-    with open("PuntosEntrega-John.txt", "w") as f:
-        for i in range(0,700):
-            if i%50==0 and i!=0:
-                f.write("\n")
-            f.write(str(PuntosEntrega[i]) + ",")
-    with open("PuntosEntrega-Miguel.txt", "w") as f:
-        for i in range(700,1400):
-            if i%50==0 and i!=700:
-                f.write("\n")
-            f.write(str(PuntosEntrega[i]) + ",")
-    with open("PuntosEntrega-Moises.txt", "w") as f:
-        for i in range(1400,2100):
-            if i%50==0 and i!=1400:
-                f.write("\n")
-            f.write(str(PuntosEntrega[i]) + ",")
-    with open("PuntosEntrega-Jorgeluis.txt", "w") as f:
-        for i in range(2100,2800):
-            if i%50==0 and i!=2100:
-                f.write("\n")
-            f.write(str(PuntosEntrega[i]) + ",")
-    with open("PuntosEntrega-Fernando.txt", "w") as f:
-        for i in range(2800,3500):
-            if i%50==0 and i!=2800:
-                f.write("\n")
-            f.write(str(PuntosEntrega[i]) + ",")
-
-def AlmacenesXintegrante():
-    with open("Almacenes.csv", "r") as f:
-        lines = f.readlines()
-        lines.pop(0)
-        Almacenes = []
-        for line in lines:
-            Almacenes.append(int(line.split(",")[0]))
-    Almacenes.sort()
-    with open("Almacenes-John.txt", "w") as f:
-        for i in range(0,14):
-            f.write(str(Almacenes[i]) + ",")
-    with open("Almacenes-Miguel.txt", "w") as f:
-        for i in range(14,28):
-            f.write(str(Almacenes[i]) + ",")
-    with open("Almacenes-Moises.txt", "w") as f:
-        for i in range(28,42):
-            f.write(str(Almacenes[i]) + ",")
-    with open("Almacenes-Jorgeluis.txt", "w") as f:
-        for i in range(42,56):
-            f.write(str(Almacenes[i]) + ",")
-    with open("Almacenes-Fernando.txt", "w") as f:
-        for i in range(56,70):
-            f.write(str(Almacenes[i]) + ",")
-
 def readPuntosEntrega(filename):
     arr=[[]for _ in range(14)]
     e=[[]for _ in range(14)]
